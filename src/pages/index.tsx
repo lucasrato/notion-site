226c894016f4f189c89b34f1e5c56a568c066291
Lucas Lima <lone.sion@gmail.com>
2020-09-07 10:15:28 -0300

    aff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default () => (
       />
       <h1>Tentando usar Notion como site</h1>
       <h2>
-        Blazing Fast Notion Blog with Next.js'{' '}
+        Carai Wendel, me da um help ai'{' '}
         <ExtLink
           href="https://github.com/vercel/next.js/issues/9524"
           className="dotted"
@@ -31,35 +31,13 @@ export default () => (
 
       <div className="explanation">
         <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>.
-          It leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
+          To tentando fazer esse negócio funcionar aqui, mas ta bem feio porque não sei editar o site direito,
+          to escrevendo o site todo no VSCode e isso não parece muito certo
         </p>
 
         <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          page's id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new page's id. To get your token from Notion, login
-          and look for a cookie under www.notion.so with the name `token_v2`.
-          After finding your token and your blog's page id you should be good to
-          go!
+         Algo está certo, porque a página está funcionando, e fiz uma página de teste ali em "Blog",
+         mas ainda não está como eu imaginava..
         </p>
       </div>
     </div>
